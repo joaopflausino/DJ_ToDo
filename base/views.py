230c9c12f59a20b41d5987f3ec75676cfd341e9c
João Pedro Flausino <joaoxpp2001@hotmail.com>
2025-02-02 15:46:51 -0300

    Updating linting and db migration

--- base/views.py
+++ base/views.py
@@ -1,1 +1,12 @@
 # views.py
+from django.shortcuts import render
+from .views_files.login_views import CustomLoginView
+from .views_files.registration_views import RegisterPage
+from .views_files.task_views import (
+    TaskList,
+    TaskDetail,
+    TaskCreate,
+    TaskUpdate,
+    DeleteView,
+    TaskReorder,
+)
